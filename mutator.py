3e89988273852e7398988bf617d3adbed8fcb04a
raghu0702 <77882741+raghu0702@users.noreply.github.com>
2021-08-01 10:42:09 +0530

    Update mutator.py

--- mutator.py
+++ mutator.py
@@ -1,11 +1,13 @@
 import random
-target_seq = input()
+target_seq = input()   #input from user(amino acid seq.)
 
 mutations = 'ARNDCQEGHILKMFPSTWYV'
-a,b = list(map(int, input("enter boundary:")))
+a,b = list(map(int, input("enter boundary:"))) #obtain a boundary as start AA, stop AA
 test_seq = target_seq[a:b]
+#mutations start
 for i in range(a,b):
     test_seq[i] = random.choice(mutations)
 
 target_seq[a:b] = test_seq
 print("Mutated")
+print(f"new sequence={target_seq}")
